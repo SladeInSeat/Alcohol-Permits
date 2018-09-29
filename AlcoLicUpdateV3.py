d32cae56c3f936c24977bf36d4899b5d6463c317
slade <sladenator78@gmail.com>
2018-09-29 15:44:29 -0400

    pep8 fixes moved clean up actions to finally

--- AlcoLicUpdateV3.py
+++ AlcoLicUpdateV3.py
@@ -1,18 +1,12 @@
-import os
 import arcpy
 import smtplib
-import string
 import traceback
 import datetime
 import StringIO
 
-
-
-
 arcpy.env.workspace = r"Database Connections\SDE@Planning_CLUSTER.sde"
 arcpy.env.overwriteOutput = True
 db_conn = r"Database Connections\SDE@Planning_CLUSTER.sde"
-
 
 #   data
 ComPlus_BusiLic = r"Database Connections\COMMPLUS.sde\COMPLUS.WPB_ALL_BUSINESSLICENSES"
@@ -29,11 +23,9 @@ Planning_Alcohol_License_fullpath = r"Database Connections\SDE@Planning_CLUSTER.
 Sql_copytable = "CATEGORY IN  ('AAM','445310','424810','312130','424820','445310','722410','312120','312140') AND STAT IN ('ACTIVE','PRINTED','HOLD')"
 
 try:
-
     # create lists of license numbers from Community Plus and corresponding table in GIS Cluster
     ComplusLicenses_set = set()
     PlanningLicenses_set = set()
-
 
     with arcpy.da.SearchCursor(ComPlus_BusiLic,'LICENSE',Sql_copytable) as ComplusUC:
         for record in ComplusUC:
@@ -48,10 +40,11 @@ try:
     InComplus_NotInSDE = ComplusLicenses_set.difference(PlanningLicenses_set)
     InSDE_NotInComplus = PlanningLicenses_set.difference(ComplusLicenses_set)
 
-    #   If InComplus_NotInSDE is not empty, then proceed to append record to GIS_ALCOHOL_LICENSES, and create a point fc of record and append to AlocholLicense_complus
+    #   If InComplus_NotInSDE is not empty, then proceed to append record to GIS_ALCOHOL_LICENSES, and create a point
+    #   fc of record and append to AlcoholLicense_complus
 
     if len(InComplus_NotInSDE) == 0:
-        with open(r"C:\Users\jsawyer\Desktop\Tickets\alcohol permits\logfile.txt","a") as log:
+        with open(r"C:\Users\jsawyer\Desktop\Tickets\alcohol permits\logfile.txt", "a") as log:
             now = datetime.datetime.now().strftime("%m-%d-%Y")
             log.write("\n-----------------\n")
             log.write(now + " no new alcohol licenses found\n\n")
@@ -59,7 +52,8 @@ try:
     else:
         arcpy.DisconnectUser(db_conn, 'ALL')
         arcpy.AcceptConnections(db_conn, False)
-        #   change list to a tuple (in prepartation of creating a text string for the query). Delta is in unicode, need it in plain ascii text for query
+        #   change list to a tuple (in prepartation of creating a text string for the query). Delta is in unicode, need
+        #   it in plain ascii text for query
 
         InComplus_NotInSDE = {x[0].encode('ascii') for x in InComplus_NotInSDE} # set comprehension to reformat to ascii
         InComplus_NotInSDE_tup = tuple(InComplus_NotInSDE)
@@ -72,14 +66,15 @@ try:
 
         arcpy.CreateTable_management(db_conn,"TempTable",Planning_AlcoLic)
 
-        with arcpy.da.SearchCursor(ComPlus_BusiLic,Fields_lessObjID,sqlquery) as sc:
+        with arcpy.da.SearchCursor(ComPlus_BusiLic,Fields_lessObjID, sqlquery) as sc:
             with arcpy.da.InsertCursor(TempTable,Fields_lessObjID) as ic:
                 for record in sc:
                     ic.insertRow(record)
 
         arcpy.Append_management(TempTable,Planning_AlcoLic)
 
-        #   THe following block creates a Query Layer from a join between the new licenses identified earlier and the parcels in which they reside, saves the Query layer as a polygon fc...
+        #   THe following block creates a Query Layer from a join between the new licenses identified earlier and the
+        #   parcels in which they reside, saves the Query layer as a polygon fc...
         #   changes that to point fc, then appends the points to AlcoholLicense_complus
 
         sql = "SELECT PARCELS.[OBJECTID],[OWNPARCELID] AS PARCELS_PCN,[SRCREF],[OWNTYPE],[GISdata_GISADMIN_OwnerParcel_AR],[LASTUPDATE],[LASTEDITOR],[Shape],[PARCEL_ID] AS COMPLUS_PCN,[BUSINESS_ID],[LICENSE],[CATEGORY],[CATEGORY_DESC],[STAT],[ISSUE],[EXPIRATION],[BUS_ENTITY_ID],[BUS_NAME],[BUS_PROD],[SERVICE],[ADRS1],[BUS_PHONE],[BUS_EMAIL] FROM [Planning].[sde].[PLANNINGPARCELS] PARCELS,[Planning].[sde].[WPB_GIS_ALCOHOL_LICENSES] ALCOLIC WHERE PARCELS.OWNPARCELID = ALCOLIC.PARCEL_ID AND {}".format(sqlquery)
@@ -88,7 +83,6 @@ try:
         arcpy.management.CopyFeatures(query_layer, alco_licence_poly, None, None, None, None)
         arcpy.FeatureToPoint_management(alco_licence_poly,alco_license_points,"INSIDE")
         arcpy.Append_management(alco_license_points,alco_license)
-
 
         #   Create the alert email text. Uses StringIO to create a string treated as a file for formatting purposes
 
@@ -101,14 +95,13 @@ try:
 
         report = string_obj.getvalue()
 
-        today =  datetime.datetime.now().strftime("%d-%m-%Y")
+        today = datetime.datetime.now().strftime("%d-%m-%Y")
         subject = 'Alcohol License report ' +  today
         sendto = ["cdglass@wpb.org","jssawyer@wpb.org"]
         sender = 'scriptmonitorwpb@gmail.com'
         sender_pw = "Bibby1997"
         server = 'smtp.gmail.com'
         body_text = "From: {0}\r\nTo: {1}\r\nSubject: {2}\r\nHere is a list of the new licenses.\nThese have been added to AlcoholLicense_complus:\n\nPCN\t\tLicense Number\tBusiness Name\tAddress\n\n{3}".format(sender, sendto, subject,report)
-
 
         gmail = smtplib.SMTP(server, 587)
         gmail.starttls()
@@ -124,13 +117,9 @@ try:
             log.write(report)
             log.write("\n")
 
+    #   This section will delete from alcohol_license_complus and Planning.SDE.WPB_GIS_ALCOHOL_LICENSES any records that
+    #   exists in Planning SDE but not in Complus (probably due to status change in complus)
 
-
-        del_list = (TempTable,alco_licence_poly,alco_license_points)
-        for fc in del_list:
-            arcpy.Delete_management(fc)
-
-  #   This section will delete from alcohol_license_complus and Planning.SDE.WPB_GIS_ALCOHOL_LICENSES any records that exists in Planning SDE but not in Complus (probably due to status change in complus)
     if len(InSDE_NotInComplus) == 0:
         print 'InSDE_NotInComplus = 0'
         print 'line 162'
@@ -181,7 +170,7 @@ try:
             log.write(now)
             log.write('\n')
             log.write('This license has been deleted:')
-            log.write((", ").join([str(obj) for obj in InSDE_NotInComplus]))
+            log.write(", ").join([str(obj) for obj in InSDE_NotInComplus])
             log.write("\n")
 
 except Exception as E:
@@ -206,3 +195,6 @@ except Exception as E:
 
 finally:
     arcpy.AcceptConnections(db_conn, True)
+    del_list = (TempTable, alco_licence_poly, alco_license_points)
+    for fc in del_list:
+        arcpy.Delete_management(fc)
